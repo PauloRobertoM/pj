250b36ac8783ba0984d9c6e866906f64b74a3e34
Paulo Alves <front@agencianovi.com.br>
2019-01-19 12:06:13 -0200

    First

---
+++ components/footer.php
@@ -0,0 +1,13 @@
+        <footer>
+            <div class="container">
+
+            </div><!-- .container -->
+        </footer>
+
+        <script type='text/javascript' src='assets/js/jquery.js'></script>
+        <script type='text/javascript' src='assets/js/bootstrap.min.js'></script>
+        <script src="https://use.fontawesome.com/6963733f46.js"></script>
+        <script type='text/javascript' src='assets/js/owl.carousel.min.js'></script>
+        <script type='text/javascript' src='assets/js/script.js'></script>
+    </body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" >
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <title>PJ</title>
+
+        <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
+        <link href="https://fonts.googleapis.com/css?family=Fira+Sans:300,400,700" rel="stylesheet">
+
+        <link rel='stylesheet' href='assets/css/bootstrap.min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/hover-min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/owl.carousel.min.css' type='text/css' />
+        <link rel='stylesheet' href='assets/css/style.css' type='text/css' />
+    </head>
+
+    <body>
+        <header>
+            <div class="nav">
+                <nav class="navbar navbar-default" id="meuMenu">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <a class="navbar-brand" href="index.php"><img class="logo" src="assets/images/logo-plast-leo.png" alt="Logo - Plast Leo" /></a>
+                        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                            <div class="container">
+                                <ul class="nav navbar-nav">
+                                    <li><a href="">Home</a></li>
+                                    <li><a href="">Quem Somos</a></li>
+                                    <li><a href="">Produtos</a></li>
+                                    <li><a href="">Clientes</a></li>
+                                    <li><a href="">Contato</a></li>
+                                </ul><!-- .nav -->
+                            </div><!-- container -->
+                        </div><!-- .navbar-collapse -->
+                    </div><!-- .navbar-header -->
+                </nav><!-- .navbar -->
+            </div><!-- .nav -->
+        </header>
